df76d799d21ed19d482be06c602494e3a22391be
t <>
2023-07-11 09:20:32 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * 作者:tom-temp
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React, { StrictMode } from "react"
+import ReactDOM from "react-dom"
+import { createRoot } from 'react-dom/client';
+// import App_first from "./01-base/aa_类组件"
+// import Box2body from "./01-base/ab.嵌套"
+// import App_ac_with_css from "./01-base/ac_组件样式"
+// import App_ad_todo from "./01-base/ad_事件绑定"
+// import App_ae_ref from "./01-base/ae_ref";
+// import App_af_state from "./01-base/af_状态";
+// import App_ag_todo from "./01-base/ag_todo_app";
+import App_ah_tab_switch from "./01-base/ah_选项卡";
+import App_ai_setstate from "./01-base/ai_setstate同步与异步";
+import App_aj_props from "./01-base/aj_属性";
+import App_ak_input from "./01-base/ak_input受控组件";
+import App_al_trans_value_to_father from "./01-base/al_子传父";
+import App_a1_input from "./02-middle/a1_表单域组件";
+import App_a2_connect from "./02-middle/a3_context通信";
+import App_life_doing_ex1 from "./02-middle/a6_生命周期_进行_案例1";
+import App_life_new from "./02-middle/a7_新生命周期1";
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // <StrictMode>
+        <App_life_new/>
+    // </StrictMode>
+    )
